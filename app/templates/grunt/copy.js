9702fe01de693edb568315777bbb213746ff2177
zsf2014 <zhang150339894@qq.com>
2015-07-17 15:56:17 +0800

    修改copy

--- app/templates/grunt/copy.js
+++ app/templates/grunt/copy.js
@@ -7,13 +7,10 @@ module.exports = {
               cwd: '<%= product.app %>',
               dest: '<%= product.dist %>',
               src: [
-                // '*.{ico,png,txt}'
-                // ,'images/{,*/}*.webp'
-                'images/{,*/}*.{jpg,png,gif}'
-                ,'*.{html,ftl}'
-                ,'view/{,*/}*.{html,ftl}'
-                ,'css/{,*/}*.css'
-                ,'scripts/*/{,*/,*/*/}*.js'
+                'images/**'
+                ,'view/**'
+                ,'css/**'
+                ,'scripts/**'
               ]
             }]
         }
